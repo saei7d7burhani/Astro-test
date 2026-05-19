ab967f509ea6395424b5065808f72a67b4ada25c
Saeid Burhani <sburhani066@gmail.com>
2026-05-19 20:08:55 +0300

    final touuches

--- src/tags/astro.config.mjs
+++ src/tags/astro.config.mjs
@@ -1,9 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
-
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://astros777d.netlify.app/',
-    integrations: [preact()]
+  integrations: [preact()]
 });
